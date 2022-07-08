a50d737e7a6bcc8a31b1571c5b5b57f642f15fa3
midgjs <sjqksk.10@gmail.com>
2022-07-08 11:55:58 +0900

    0708 오전까지

--- src/main/java/com/javaex/dao/BlogDao.java
+++ src/main/java/com/javaex/dao/BlogDao.java
@@ -4,11 +4,19 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.javaex.vo.BlogVo;
+
 @Repository
 public class BlogDao {
 	
 	@Autowired
 	SqlSession sqlSession;
 	
-	
+	public BlogVo visitblog(BlogVo blogVo) {
+		System.out.println("blogDao > visitblog");
+		
+		BlogVo blogid = sqlSession.selectOne("blog.visitblog", blogVo);
+		
+		return blogid;
+	}
 }
